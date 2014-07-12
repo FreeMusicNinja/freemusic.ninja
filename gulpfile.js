d89a4a08bad6198d413dec769aaa8d57f447a87b
Trey Hunner <trey@treyhunner.com>
2014-07-12 11:58:13 -0700

    Reload express server on change and handle errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
 /* global require */
 
 var gulp = require('gulp'),
-    spawn = require('child_process').spawn;
+    spawn = require('child_process').spawn,
+    node;
 
 
-gulp.task('server', function() {
-  spawn('node', ['node/server.js'], {stdio: 'inherit'});
+gulp.task('server', function () {
+  if (node) node.kill();  // Kill server if one is running
+  node = spawn('node', ['node/server.js'], {stdio: 'inherit'});
+  node.on('close', function (code) {
+    if (code === 8) {
+      console.log('Error detected, waiting for changes...');
+    }
+  });
 });
 
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   gulp.run('server');
+
+  // Reload server if files changed
+  gulp.watch(['node/**/*.js'], function () {
+    gulp.run('server');
+  });
+});
+
+
+// kill node server on exit
+process.on('exit', function() {
+    if (node) node.kill();
 });
