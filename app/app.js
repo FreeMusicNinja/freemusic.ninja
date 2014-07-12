8ed98ea452f7f39a087c5ef192876bb6ca99ba8a
Trey Hunner <trey@treyhunner.com>
2014-07-12 11:18:30 -0700

    Use generator to make REST adapter

--- app/app.js
+++ app/app.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import DS from 'ember-data';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 
@@ -8,10 +7,6 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 var App = Ember.Application.extend({
   modulePrefix: 'musicfinder', // TODO: loaded via config
   Resolver: Resolver
-});
-
-DS.RESTAdapter.reopen({
-  namespace: 'api'
 });
 
 loadInitializers(App, 'musicfinder');
