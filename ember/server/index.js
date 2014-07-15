750cfc4ad09b10f68d57e35248f1b047ead77ddd
Trey Hunner <trey@treyhunner.com>
2014-07-15 00:22:51 -0700

    Move back-end api code to node package

--- ember/server/index.js
+++ ember/server/index.js
@@ -7,8 +7,6 @@
 //   });
 // };
 
-require('./db');
-
 var bodyParser = require('body-parser');
 var globSync   = require('glob').sync;
 var routes     = globSync('./routes/**/*.js', { cwd: __dirname }).map(require);
--- node/server.js
+++ node/server.js
@@ -1,9 +1,21 @@
 /* global require */
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var globSync   = require('glob').sync;
+
+require('./app/db');
+
 var app = express();
+var routes = globSync('./app/routes/**/*.js', { cwd: __dirname }).map(require);
+
 app.listen(3900, function() {
   console.log('Listening on port 3900');
 });
 
-exports = module.exports = app;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+routes.forEach(function(route) { route(app); });
