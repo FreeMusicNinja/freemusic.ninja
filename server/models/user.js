788fd799cd83cb2398abaaa2f9446fe5186fa0b5
Trey Hunner <trey@treyhunner.com>
2014-07-19 23:48:40 -0700

    Add User model with bcrypt password encryption

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var SALT_WORK_FACTOR = 10;
+
+var UserSchema = new Schema({
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+});
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if(!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
+
+mongoose.model('User', UserSchema);
