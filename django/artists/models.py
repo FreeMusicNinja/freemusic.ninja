090609d90ceed1b10aac191f278f64d734ae9a87
Trey Hunner <trey@treyhunner.com>
2014-09-12 19:55:53 -0700

    Add models for Magnatune and Jamendo artists

--- django/artists/models.py
+++ django/artists/models.py
@@ -6,4 +6,31 @@ class Artist(TimeStampedModel):
 
     """Music artist served over the API."""
 
-    name = models.CharField(max_length=300, unique=True)
+    name = models.CharField(max_length=100, unique=True)
+
+
+class JamendoArtist(TimeStampedModel):
+
+    """Music artist data stored from the Jamendo API."""
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    website = models.URLField()
+    joindate = models.DateField()
+    image = models.URLField()
+    shorturl = models.URLField()
+    shareurl = models.URLField()
+
+
+class MagnatuneArtist(TimeStampedModel):
+
+    """Music artist data stored from the Magnatune sqlite database."""
+
+    artist = models.CharField(max_length=100, unique=True)
+    description = models.TextField(max_length=500)
+    bio = models.TextField(max_length=12000)
+    homepage = models.CharField(max_length=100)
+    bandphoto = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
