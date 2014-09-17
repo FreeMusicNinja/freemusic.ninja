378f12b93a1917c7e4c6d414941f1f898bb71712
Trey Hunner <trey@treyhunner.com>
2014-09-17 14:08:45 -0700

    Move gulpfile from Node to Django

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,25 @@
 
 var gulp = require('gulp');
 var spawn = require('child_process').spawn;
-var ember, node, mocha;
+var ember, django, mocha;
 var installs = [];
 
-var NODE_PORT = 3900,
+var DJANGO_PORT = 3900,
     EMBER_PORT = 4900;
 
 
-gulp.task('node', function () {
-  if (node) node.kill();  // Kill server if one is running
-  process.env.PORT = NODE_PORT;
-  node = spawn('node', ['node/index.js'], {stdio: 'inherit'});
-  node.on('close', function (code) {
+gulp.task('django', function () {
+  if (django) django.kill();  // Kill server if one is running
+  django = spawn('./django/manage.py', ['runserver', DJANGO_PORT], {stdio: 'inherit'});
+  django.on('close', function (code) {
     if (code === 8) {
-      console.log('Node error detected, waiting for changes...');
+      console.log('Django error detected, waiting for changes...');
     }
   });
 });
 
 gulp.task('ember', function () {
-  process.env.API_URL = "http://localhost:" + NODE_PORT + "/";
+  process.env.API_URL = "http://localhost:" + DJANGO_PORT + "/";
   ember = spawn('./node_modules/.bin/ember', ['server', '--port=' + EMBER_PORT,], {cwd: 'ember', stdio: 'inherit'});
   ember.on('close', function (code) {
     if (code === 8) {
@@ -39,25 +38,19 @@ gulp.task('install', function () {
 
 
 gulp.task('test', function () {
-  process.env.NODE_ENV = 'test';
-  mocha = spawn('./node_modules/mocha/bin/mocha', ['node/tests', '--require=node/index.js'], {stdio: 'inherit'});
+  process.env.DJANGO_ENV = 'test';
+  mocha = spawn('./django/manage.py', ['test'], {stdio: 'inherit'});
 });
 
 
 gulp.task('default', function () {
-  gulp.run('node');
-  gulp.run('ember');
-
-  // Reload node if files changed
-  gulp.watch(['node/**/*.js'], function () {
-    gulp.run('node');
-  });
+  gulp.run('test');
 });
 
 
-// kill node server on exit
+// kill servers on exit
 process.on('exit', function() {
-    if (node) node.kill();
+    if (django) django.kill();
     if (ember) ember.kill();
     if (mocha) mocha.kill();
     installs.forEach(function (install) { install.kill(); });
