36c755e7baf8cdffd1f81077366603a58485786f
Trey Hunner <trey@treyhunner.com>
2014-09-22 16:09:00 -0700

    Merge /similar/ and /artists/ endpoints

--- django/freemusicninja/urls.py
+++ django/freemusicninja/urls.py
@@ -3,14 +3,12 @@ from django.contrib import admin
 from rest_framework import routers
 
 from artists.views import ArtistViewSet
-from similarities.views import SimilarArtistList
 
 router = routers.DefaultRouter()
 router.register(r'artists', ArtistViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'similar', SimilarArtistList.as_view()),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
