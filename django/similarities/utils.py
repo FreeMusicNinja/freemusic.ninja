e8d67354706d5b99f718aa6839db0b0fbec1b31e
Trey Hunner <trey@treyhunner.com>
2014-09-28 12:15:51 -0700

    Add tests for similarities.utils

--- django/similarities/utils.py
+++ django/similarities/utils.py
@@ -8,29 +8,35 @@ from .models import (GeneralArtist, UserSimilarity, Similarity,
                      update_similarities)
 
 
-def add_new_similarities(artist, force_update=False):
-    similarities = []
-    responses = SimilarResponse.objects.filter(
-        normalized_name=artist.normalized_name)
-    if responses.exists() and not force_update:
-        return  # Echo Nest similarities already added
+def has_similarities(general_artist):
+    """Return True EchoNest similarities were already added."""
+    return SimilarResponse.objects.filter(
+        normalized_name=general_artist.normalized_name).exists()
+
+
+def make_similarity(echonest_user, other_artist, cc_artist):
+    """Make EchoNest similarity between general artist and CC artist."""
+    kwargs = {'other_artist': other_artist, 'cc_artist': cc_artist}
+    UserSimilarity.objects.get_or_create(defaults={'weight': 1},
+                                         user=echonest_user, **kwargs)
+    return Similarity.objects.get_or_create(**kwargs)[0]
+
+
+def add_new_similarities(artist):
+    """Add new CC artist similarities for the given artist."""
     user = User.objects.get(email='echonest')
     artist_names = echonest.get_similar(artist.name)
     cc_artists = Artist.objects.filter(name__in=artist_names)
-    for cc_artist in cc_artists:
-        kwargs = dict(
-            cc_artist=cc_artist,
-            other_artist=artist,
-        )
-        UserSimilarity.objects.get_or_create(defaults={'weight': 1},
-                                             user=user, **kwargs)
-        similarities.append(Similarity.objects.get_or_create(**kwargs)[0])
-    update_similarities(similarities)
+    update_similarities(
+        make_similarity(user, artist, cc_artist)
+        for cc_artist in cc_artists
+    )
 
 
 def get_similar(name):
     artist, _ = GeneralArtist.objects.get_or_create(
         normalized_name=name.upper(), defaults={'name': name})
-    add_new_similarities(artist)
+    if not has_similarities(artist):
+        add_new_similarities(artist)
     similar = Q(similarity__other_artist=artist, similarity__weight__gt=0)
     return Artist.objects.filter(similar).order_by('-similarity__weight')
