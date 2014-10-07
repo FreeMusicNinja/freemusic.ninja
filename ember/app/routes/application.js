a70cbd5207a4f168057ad972dfd6e914c2c52373
Trey Hunner <trey@treyhunner.com>
2014-10-07 14:22:16 -0700

    Add a (buggy) welcome message to user

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  model: function () {
+    if (this.get('session.isAuthenticated')) {
+      var authController = this.controllerFor('auth');
+      authController.set('model', this.store.find('user', 'me'));
+    }
+  },
+});
