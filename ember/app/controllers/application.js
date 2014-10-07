f187e1593ef6aba6179f8177fdc9d07b8342e722
Trey Hunner <trey@treyhunner.com>
2014-10-07 16:42:51 -0700

    Hide Logout link and greeting until user retrieved

--- ember/app/controllers/application.js
+++ ember/app/controllers/application.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     needs: ['auth'],
-    userNameBinding: 'controllers.auth.content.name'
+    userNameBinding: 'controllers.auth.content.name',
+    isLoggedIn: function() {
+      return !Ember.isEmpty(this.get('userName'));
+    }.property('userName'),
 });
