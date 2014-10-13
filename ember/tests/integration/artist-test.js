20125c05ae9f33442e47f46595062556c244f3cc
Trey Hunner <trey@treyhunner.com>
2014-10-12 21:27:02 -0700

    Add test similar artist result

--- ember/tests/integration/artist-test.js
+++ ember/tests/integration/artist-test.js
@@ -16,11 +16,28 @@ module("Artist recommendation tests", {
 test("no similar artists", function() {
   expect(1);
   $.mockjax({
-    url: 'http://api/artists?name=Unknown',
+    url: 'http://api/artists/',
     contentType: 'application/json',
     responseText: [],
   });
   visit('/artists?name=Unknown').then(function() {
     equal(find('.panel').text().trim(), 'No artist found.', "Empty artist results text not found.");
   });
+});
+
+test("similar artists via URL", function() {
+  expect(1);
+  $.mockjax({
+    url: 'http://api/artists/',
+    contentType: 'application/json',
+    responseText: [{
+      id: 1,
+      name: "Jonathan Coulton",
+      links: [],
+    }],
+    onAfterComplete: function () {
+      equal(find('.panel').text().trim(), 'Jonathan Coulton', "Artist results not found.");
+    },
+  });
+  visit('/artists?name=Weird+Al+Yankovic');
 });
