46b41fdd1b62b2cc373a1923b66259418fdf61ad
Trey Hunner <trey@treyhunner.com>
2014-11-10 19:53:47 -0800

    Add 0 rating for artists

--- app/models/similarity.js
+++ app/models/similarity.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   cc_artist: DS.belongsTo('artist'),
   other_artist: DS.attr(),
   weight: DS.attr(),
-  weights: [1, 2, 3, 4, 5],
+  weights: [0, 1, 2, 3, 4, 5],
 });
