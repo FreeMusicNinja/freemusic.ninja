eb0870271f30be27fb0463809c4ebc97eaeb4989
Trey Hunner <trey@treyhunner.com>
2014-11-29 14:56:48 -0800

    Show only correct similarities on artist page

--- app/controllers/artist/index.js
+++ app/controllers/artist/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   similarities: function () {
-    return this.store.find('similarity', {artist: this.get('content.id')});
+    return this.store.find('similarity', {cc_artist: this.get('content.id')});
   }.property('content'),
 });
