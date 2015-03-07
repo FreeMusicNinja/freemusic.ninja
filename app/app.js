50c263b2c0328a86e0291296f23694b8417531e6
Trey Hunner <trey@treyhunner.com>
2015-03-06 19:30:23 -0800

    Upgrade ember-cli

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
-Ember.onerror = Ember.K; // FIXME this is a workaround
 
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
